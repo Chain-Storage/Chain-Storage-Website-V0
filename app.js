add781c947bc2542efc4dd7408e94ecd52c99b6e
yurikaza <yusufmirza55@hotmail.com>
2022-01-15 10:17:18 +0300

    heroku post build code added

--- app.js
+++ app.js
@@ -35,12 +35,13 @@ app.use(xss());
 
 app.use("/api/v1/users", usersRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
